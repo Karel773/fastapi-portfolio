e87af180c6c4122864647cc3c933c1f5a875c124
Karel773 <karolnowicki2005@gmail.com>
2025-12-25 21:43:23 +0100

    Add User and Task models

---
+++ models.py
@@ -0,0 +1,20 @@
+from sqlalchemy import Column, Integer, String, Boolean 
+from database import Base # import base from database.py 
+
+# create a users table
+class User(Base): 
+    __tablename__ = "users" 
+
+    id = Column(Integer, primary_key = True, index = True) 
+    name = Column(String, nullable = False) 
+    is_active = Column(Boolean, default = True) 
+
+# create a tasks table 
+class Task(Base):
+    __tablename__ = "users" 
+
+    id = Column(Integer, primary_key = True, index = True) 
+    title = Column(String, nullable = False) 
+    description = Column(String, nullable = False) 
+    completed = Column(Boolean, default = False) 
+    user_id = Column(Integer, nullable = False) 
