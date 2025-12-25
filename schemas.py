aca8007de24bbf1ecc9699d0a12e58023ed6b18d
Karel773 <karolnowicki2005@gmail.com>
2025-12-25 21:52:23 +0100

    Add data validation with Pydantic Field

---
+++ schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, Field
+from typing import Optional  
+
+class UserCreate(BaseModel): 
+    name: str = Field(..., min_length= 2, max_length = 50, description = "Username must be 2-50 characters long")
+
+class UserPublic(BaseModel):
+    id: int 
+    name: str 
+    is_active: bool 
+
+    class Config:
+        orm_mode = True 
+
+class TaskCreate(BaseModel):
+    title: str = Field(..., min_length = 1, max_length = 100) 
+    description: Optional[str] = Field(default = None, max_length = 300) 
+    user_id: int = Field(..., gt = 0)
+
+class TaskPublic(BaseModel):
+    id: int 
+    title: str 
+    description: Optional[str]
+    completed: bool 
+    user_id: int 
+
+    class Config:
+        orm_mode = True 
